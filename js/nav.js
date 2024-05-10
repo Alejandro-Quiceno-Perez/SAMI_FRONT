25406b4dee87c7c9ec63b9771ead8ddd0f44d861
Alejo Perez <alejo.perez2905@gmail.com>
2024-05-10 12:15:40 -0500

    rutes update

--- js/nav.js
+++ js/nav.js
@@ -3,22 +3,22 @@ function nav(){
     navContainer.innerHTML = `
                 <ul class="nav">
                 <li>
-                    <img src="../img/logo3BG.png" alt="" class="img-logo-header">
+                    <img src="../front/img/logo3BG.png" alt="" class="img-logo-header">
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-danger" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-danger" href="../pages/userPage.html">Your Service</a>
+                    <a class="nav-link text-danger" href="../front/pages/userPage.html">Your Service</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-danger" href="../pages/aboutUs.html">About Us</a>
+                    <a class="nav-link text-danger" href="../front/pages/aboutUs.html">About Us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-whit" aria-disabled="true" href="../pages/admin.html">Admin</a>
+                    <a class="nav-link text-whit" aria-disabled="true" href="../front/pages/admin.html">Admin</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-whit" aria-disabled="true" href="../pages/ambulance.html">Ambulance</a>
+                    <a class="nav-link text-whit" aria-disabled="true" href="../front/pages/ambulance.html">Ambulance</a>
                 </li>
             </ul>
             <ul class="nav">
@@ -26,7 +26,7 @@ function nav(){
                             Cuchumfulo</a></i></li>
                 <li class="nav-item d-flex me-5"><a href="" class="nav-link text-danger">ES</a><span
                         class="nav-link text-danger">/</span><a href="" class="nav-link text-danger">EN</a></li>
-                <li class="nav-item"><a class="btn btn-danger" href="../pages/login.html">Login</a></li>
+                <li class="nav-item"><a class="btn btn-danger" href="../front/pages/login.html">Login</a></li>
                 <li class="nav-item d-none"><a class="btn btn-danger" href="../pages/login.html">Log out</a></li>
             </ul>
         `;
